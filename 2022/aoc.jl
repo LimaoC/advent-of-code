a949cbc2edb058621367a1f5c8ff52e414be9f0d
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2022-12-11 14:58:23 +1000

    2022: refactor day1-9 with AdventOfCode functions

--- 2022/aoc.jl
+++ 2022/aoc.jl
@@ -1,16 +1,18 @@
-function one()
-    # https://adventofcode.com/2022/day/1
-    lines = split(open(f -> read(f, String), "day1_input.txt"), "\n\n", keepempty=false)
-    sums = map(line -> map(arr -> parse.(Int64, arr), split(line, "\n", keepempty=false)), lines)
-    sums = map(arr -> sum(arr), sums)
+using Pkg; Pkg.activate(".")
+using AdventOfCode
+
+"""https://adventofcode.com/2022/day/1"""
+function one(; input::String = "2022/day1_input.txt")
+    data = intvec2(input)
+    sums = map(v -> sum(v), data)
     sort!(sums)
     println(sums[end])
     println(sum(sums[end-2:end]))
 end
 
-function two()
-    # https://adventofcode.com/2022/day/2
-    lines = split(open(f -> read(f, String), "day2_input.txt"), "\n", keepempty=false)
+"""https://adventofcode.com/2022/day/2"""
+function two(; input::String = "2022/day2_input.txt")
+    data = strvec(input)
     d1 = Dict(  # shape + outcome
         "A X" => 1 + 3, "A Y" => 2 + 6, "A Z" => 3 + 0,
         "B X" => 1 + 0, "B Y" => 2 + 3, "B Z" => 3 + 6,
@@ -21,82 +23,69 @@ function two()
         "B X" => 1 + 0, "B Y" => 2 + 3, "B Z" => 3 + 6,
         "C X" => 2 + 0, "C Y" => 3 + 3, "C Z" => 1 + 6
     )
-    score1 = sum([d1[line] for line in lines])
-    score2 = sum([d2[line] for line in lines])
-    println(score1)
-    println(score2)
+    println(sum([d1[x] for x in data]))
+    println(sum([d2[x] for x in data]))
 end
 
-function three()
-    # https://adventofcode.com/2022/day/3
-    lines = split(open(f -> read(f, String), "day3_input.txt"), "\n", keepempty=false)
-    priority(chr) = chr in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ? Int(chr) - 38 : Int(chr) - 96
-    total1 = total2 = 0
-    for line in lines
-        first, last = line[begin:length(line)÷2], line[length(line)÷2+1:end]
-        # find common item
-        for chr in first
-            if chr in last
-                total1 += priority(chr)
-                break
-            end
-        end
+"""https://adventofcode.com/2022/day/3"""
+function three(; input::String = "2022/day3_input.txt")
+    data = strvec(input)
+    priority(chr) = isuppercase(chr) ? Int(chr) - 38 : Int(chr) - 96
+    total1 = 0
+    for line in data
+        first, second = splitequal(line, 2)
+        total1 += priority(intersect(first, second)[1])
     end
-    lines3 = map(n -> (lines[n], lines[n+1], lines[n+2]), 1:3:length(lines))
-    for (first, second, third) in lines3
-        # find common item
-        for chr in first
-            if chr in second && chr in third
-                total2 += priority(chr)
-                break
-            end
-        end
+    data = map(n -> (data[n], data[n+1], data[n+2]), 1:3:length(data))
+    total2 = 0
+    for (first, second, third) in data
+        total2 += priority(intersect(first, second, third)[1])
     end
     println(total1)
     println(total2)
 end
 
-function four()
-    # https://adventofcode.com/2022/day/4
-    lines = split(open(f -> read(f, String), "day4_input.txt"), "\n", keepempty=false)
+"""https://adventofcode.com/2022/day/4"""
+function four(; input::String = "2022/day4_input.txt")
+    data = strvec(input)
     count1 = count2 = 0
-    for line in lines
+    for line in data
         first, second = split(line, ',')
         f1, f2 = parse.(Int, split(first, '-'))
         s1, s2 = parse.(Int, split(second, '-'))
         # check either range fully contains the other
-        if (f1 in s1:s2 && f2 in s1:s2) || (s1 in f1:f2 && s2 in f1:f2)
-            count1 += 1
-        end
+        ((f1 in s1:s2 && f2 in s1:s2) || (s1 in f1:f2 && s2 in f1:f2)) && (count1 += 1)
         # check either range overlaps
-        if (f1 in s1:s2 || f2 in s1:s2) || (s1 in f1:f2 || s2 in f1:f2)
-            count2 += 1
-        end
+        ((f1 in s1:s2 || f2 in s1:s2) || (s1 in f1:f2 || s2 in f1:f2)) && (count2 += 1)
     end
     println(count1)
     println(count2)
 end
 
-function five()
-    # https://adventofcode.com/2022/day/5
+"""https://adventofcode.com/2022/day/5"""
+function five(; input::String = "2022/day5_input.txt")
     # parse input
-    lines = split(open(f -> read(f, String), "day5_input.txt"), "\n", keepempty=true)
-    n = findfirst(x -> isempty(x), lines)
-    crate_lines = lines[1:n-2]
-    crate_range = 2:4:length(lines[1])
+    data = strvec(input)
+    n = findfirst(line -> startswith(line, "move"), data)
+    crate_data = data[1:n-2]
+    crate_range = 2:4:length(data[1])
     crates1 = [[] for _ in 1:length(crate_range)]
-    for crate_line in crate_lines
-        for (index, chr) in enumerate(crate_line[crate_range])
-            chr != ' ' && pushfirst!(crates1[index], chr)
+    crates2 = [[] for _ in 1:length(crate_range)]
+    for crate in crate_data
+        for (index, chr) in enumerate(crate[crate_range])
+            chr != ' ' && (pushfirst!(crates1[index], chr); pushfirst!(crates2[index], chr))
         end
     end
-    crates2 = deepcopy(crates1)
     # move crates
-    for move in lines[n+1:end-1]
+    for move in data[n:end]
         _, repetitions, _, src, _, dest = tryparse.(Int, split(move, " "))
-        temp = []  # to reverse all moved crates in this move at once
+        # part 1
         for _ in 1:repetitions
             push!(crates1[dest], pop!(crates1[src]))
+        end
+        # part 2
+        temp = []
+        for _ in 1:repetitions
             pushfirst!(temp, pop!(crates2[src]))
         end
         push!(crates2[dest], temp...)
@@ -107,14 +96,14 @@ function five()
     println()
 end
 
-function six()
-    # https://adventofcode.com/2022/day/6
-    string = open(f -> read(f, String), "day6_input.txt")[begin:end-1]
+"""https://adventofcode.com/2022/day/6"""
+function six(; input::String = "2022/day6_input.txt")
+    data = strsingle(input)
     is_unique(s) = length(unique(s)) == length(s)
     offsets = (3, 13)  # message length - 1
     for offset in offsets
-        for index in 1:length(string)-offset
-            if is_unique(string[index:index+offset])
+        for index in 1:length(data)-offset
+            if is_unique(data[index:index+offset])
                 println(index + offset)
                 break
             end
@@ -122,14 +111,14 @@ function six()
     end
 end
 
-function seven()
-    # https://adventofcode.com/2022/day/7
-    commands = split(open(f -> read(f, String), "day7_input.txt"), "\$ ", keepempty=false)
+"""https://adventofcode.com/2022/day/7"""
+function seven(; input::String = "2022/day7_input.txt")
+    data = strvec(input, "\$ ")
     root = [0, Dict()]
     current_path = []
     current_dir = root
-    for command in commands
-        lines = split(command, "\n", keepempty=false)
+    for cmd in data
+        lines = split(cmd, "\n", keepempty=false)
         if lines[1] == "ls"
             for output in lines[2:end]
                 type, name = split(output, " ", keepempty=false)
@@ -194,39 +183,41 @@ function seven()
     println(minimum(dirs))
 end
 
-function eight()
-    # https://adventofcode.com/2022/day/8
-    lines = split(open(f -> read(f, String), "day8_input.txt"), "\n", keepempty=false)
-    lines = map(line -> parse.(Int64, split(line, "", keepempty=false)), lines)
-    trees = reduce(hcat, lines)'
+"""https://adventofcode.com/2022/day/8"""
+function eight(; input::String = "2022/day8_input.txt")
+    data = intmatrix(input)
 
     function is_visible(row, col)  # part 1
-        rowv, colv = trees[row, :], trees[:, col]
+        rowv, colv = data[row, :], data[:, col]
         dirs = (colv[begin:row-1], colv[row+1:end], rowv[begin:col-1], rowv[col+1:end])
-        return any(map(dir -> all(trees[row, col] .> dir), dirs))
+        return any(map(dir -> all(data[row, col] .> dir), dirs))
     end
 
     function scenic_score(row, col)  # part 2
         scores = [0, 0, 0, 0]
-        rowv, colv = trees[row, :], trees[:, col]
-        dirs = (reverse(colv[begin:row-1]), colv[row+1:end], reverse(rowv[begin:col-1]), rowv[col+1:end])
+        rowv, colv = data[row, :], data[:, col]
+        dirs = (
+            reverse(colv[begin:row-1]), colv[row+1:end],
+            reverse(rowv[begin:col-1]), rowv[col+1:end]
+        )
         for (index, dir) in enumerate(dirs)
             for tree in dir
                 scores[index] += 1
-                tree >= trees[row, col] && break
+                tree >= data[row, col] && break
             end
         end
         return prod(scores)
     end
 
-    println(sum([is_visible(i[1], i[2]) for i in eachindex(trees)]))
-    println(maximum([scenic_score(i[1], i[2]) for i in eachindex(trees)]))
+    dim = size(data)[1]
+    data = view(data, 1:dim, 1:dim)  # to use Cartesian indexing instead of linear indexing
+    println(sum([is_visible(i[1], i[2]) for i in eachindex(data)]))
+    println(maximum([scenic_score(i[1], i[2]) for i in eachindex(data)]))
 end
 
-function nine()
-    # https://adventofcode.com/2022/day/9
-    lines = split(open(f -> read(f, String), "day9_input.txt"), "\n", keepempty=false)
-
+"""https://adventofcode.com/2022/day/9"""
+function nine(; input::String = "2022/day9_input.txt")
+    data = strvec(input)
     UP, DOWN, LEFT, RIGHT = (0, 1), (0, -1), (-1, 0), (1, 0)
     NE, SE, SW, NW = (1, 1), (1, -1), (-1, -1), (-1, 1)
 
@@ -260,8 +251,8 @@ function nine()
     # part 2
     visited2 = Set([(0, 0)])
     nodes2 = [(0, 0) for _ in 1:10]
-    for line in lines
-        direction, units = split(line, " ")
+    for cmd in data
+        direction, units = split(cmd, " ")
         direction = move_delta(direction)
         units = parse(Int64, units)
         for _ in 1:units
